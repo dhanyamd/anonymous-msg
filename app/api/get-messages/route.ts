bd9fc6acc62c0020bc4a6c2b97bbdab4af11b7cd
Dhanya MD <dhanya13md@gmail.com>
2024-06-11 23:45:36 +0530

    added get-msg-route

---
+++ app/api/get-messages/route.ts
@@ -0,0 +1,42 @@
+import { getServerSession} from "next-auth";
+import UserModel from "@/models/User";
+import dbConnect from "@/lib/dbConnect";
+import { User } from "next-auth";
+import { AuthOptions } from "../auth/[...nextauth]/options";
+import mongoose from "mongoose";
+
+export async function GET(request: Request){
+    await dbConnect();
+
+    const session = await getServerSession(AuthOptions) 
+    const user: User = session?.user
+    if(!session && !user){
+        return Response.json({
+            success: false,
+            message: "Not authenticated"
+        },{status: 401})
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try{
+     const user = await UserModel.aggregate([
+        {$match: {id : userId}},
+        {$unwind: '$messages'},
+        {$sort: {'messages.createdAt': -1}},
+        {$group: {_id: '$_id', messages: {$push: '$messages'}}}
+     ])
+     if(!user || user.length === 0){
+        return Response.json({
+            success: false,
+            message: "User not found"
+        },{status: 401})
+     }
+     return Response.json({
+        success: true,
+        messages : user[0].messages
+     },{status : 200})
+    }catch(error){
+
+    }
+ 
+}
---
+++ app/api/send-messages/route.ts
@@ -0,0 +1,7 @@
+import UserModel from "@/models/User";
+import dbConnect from "@/lib/dbConnect";
+import { Message } from "@/models/User";
+
+export async function POST(){
+
+}
